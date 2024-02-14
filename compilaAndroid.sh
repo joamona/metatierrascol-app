c2875f40f13f7a15b46faaba6165dbea451104b7
joamona <joamona@cgf.upv.es>
2024-02-14 14:23:41 +0100

    m

--- compilaAndroid.sh
+++ compilaAndroid.sh
@@ -1,2 +1,4 @@
 export CAPACITOR_ANDROID_STUDIO_PATH="/home/joamona/desktopApps/android-studio-2023.1.1.28-linux/android-studio/bin/studio.sh"
 ionic cap build android
+npx cap sync
+npx cap open android
